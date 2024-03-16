731e2e5923853cbcf4370bbdc5b2d000c58beab8
Epixl Studio <abdalrahimk083@gmail.com>
2024-03-17 00:42:36 +0300

    adding swiper & resposive & nav-menu-button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,47 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  NgZone,
+  ViewEncapsulation,
+} from '@angular/core';
+
+import SwiperCore, {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  SwiperOptions,
+} from 'swiper';
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
-  title = 'Etmaam';
+  title: string = 'Etmaam';
+  isMenuOpen: boolean = false;
 
-  slides: { title: string; items: string[] }[] = [
+  config: SwiperOptions = {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    navigation: true,
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      992: {
+        slidesPerView: 3,
+      },
+    },
+  };
+
+  slidesData: { title: string; items: string[]; mainDivClasses: string }[] = [
     {
       title: 'ما قبل التأسيس',
       items: [
@@ -18,6 +51,8 @@ export class AppComponent {
         'اثبات الهوية و اثبات العنوان للمدير و المعاونين',
         'نسخة مدققة للتفويض الكتابي',
       ],
+      mainDivClasses:
+        'mx-3 px-6 py-9 text-sky-800 shadow-md border border-sky-800',
     },
     {
       title: 'الوثائق المطلوبة لعملية الحجز',
@@ -28,6 +63,7 @@ export class AppComponent {
         'اثبات الهوية و اثبات العنوان للمدير و المعاونين',
         'نسخة مدققة للتفويض الكتابي',
       ],
+      mainDivClasses: 'mx-3 px-6 py-10 text-sky-800 shadow-md',
     },
     {
       title: 'تأسيس النشاط',
@@ -38,6 +74,7 @@ export class AppComponent {
         'اثبات الهوية و اثبات العنوان للمدير و المعاونين',
         'نسخة مدققة للتفويض الكتابي',
       ],
+      mainDivClasses: 'mx-3 px-6 py-10 text-sky-800 shadow-md',
     },
     {
       title: 'الوثائق المطلوبة للتأسيس',
@@ -48,10 +85,16 @@ export class AppComponent {
         'اثبات الهوية و اثبات العنوان للمدير و المعاونين',
         'نسخة مدققة للتفويض الكتابي',
       ],
+      mainDivClasses: 'mx-3 px-6 py-10 text-sky-800 shadow-md',
+    },
+    {
+      title: '',
+      items: [],
+      mainDivClasses: '',
     },
   ];
 
-  items: string[] = [
+  links: string[] = [
     'أطلب خدمة',
     'من نحن؟',
     'الأسئلة الشائعة',
@@ -59,36 +102,118 @@ export class AppComponent {
     'انضم إلينا',
   ];
 
-  servicesSlides = [
+  servicesSlides: {
+    classes: string;
+    content: {
+      imageSrc: string;
+      imageAlt: string;
+      title: string;
+      description: string;
+    };
+  }[] = [
     {
-      title: 'الخدمات القانونية | الالتزام القانوني',
-      image: '../assets/svg/low.svg',
-      description:
-        'نحن نمتلك أفضل فريق قانوني في المملكة العربية السعودية مع سنوات من الخبرة والمعرفة، ونقدمها لك لتستفيد وتتمكن من رؤية السوق بمنظور مختلف.',
+      classes:
+        'px-5 py-10 text-sky-900 shadow-lg max-w-96 min-h-96 border border-slate-100',
+      content: {
+        imageSrc: '../assets/svg/low.svg',
+        imageAlt: 'low',
+        title: 'الخدمات القانونية | الالتزام القانوني',
+        description:
+          'نحن نمتلك أفضل فريق قانوني في المملكة العربية السعودية مع سنوات من الخبرة والمعرفة، ونقدمها لك لتستفيد وتتمكن من رؤية السوق بمنظور مختلف.',
+      },
     },
     {
-      title: 'تصفية الشركة',
-      image: '../assets/svg/note.svg',
-      description:
-        'نحن في خدمتكم لتوفير حلول مبتكرة وسهلة التنفيذ التي تساعد على تحقيق نجاح شركتكم بكل يسر وسلاسة، دون مواجهة أي مشاكل أو عقبات.',
+      classes:
+        'px-5 py-10 shadow-lg max-w-96 min-h-96 bg-gradient-to-b from-sky-600 to-blue-900 text-white',
+      content: {
+        imageSrc: '../assets/svg/note.svg',
+        imageAlt: 'note',
+        title: 'تصفية الشركة',
+        description:
+          'نحن في خدمتكم لتوفير حلول مبتكرة وسهلة التنفيذ التي تساعد على تحقيق نجاح شركتكم بكل يسر وسلاسة، دون مواجهة أي مشاكل أو عقبات.',
+      },
     },
     {
-      title: 'خدمات الترجمة و تحسين التواصل',
-      image: '../assets/svg/translate.svg',
-      description:
-        'نحن نقدم خدمات شاملة لتحسين التواصل ونساعدك على الحصول على أفضل صفقة وبناء علاقات ناجحة في مجال عملك.',
+      classes:
+        'px-5 py-10 text-sky-900 shadow-lg max-w-96 min-h-96 border border-slate-100',
+      content: {
+        imageSrc: '../assets/svg/translate.svg',
+        imageAlt: 'translate',
+        title: 'خدمات الترجمة و تحسين التواصل',
+        description:
+          'نحن نقدم خدمات شاملة لتحسين التواصل ونساعدك على الحصول على أفضل صفقة وبناء علاقات ناجحة في مجال عملك.',
+      },
     },
     {
-      title: 'الفيزا و التأشيرات في المملكة العربية السعودية',
-      image: '../assets/svg/visa.svg',
-      description:
-        'نحن نقدم خدمات استشارية تساعدكم على اختيار التأشيرة الصحيحة ونقدم المساعدة في استخراجها بسهولة ويسر.',
+      classes:
+        'px-5 py-10 text-sky-900 shadow-lg max-w-96 min-h-96 border border-slate-100',
+      content: {
+        imageSrc: '../assets/svg/visa.svg',
+        imageAlt: 'visa',
+        title: 'الفيزا و التأشيرات في المملكة العربية السعودية',
+        description:
+          'نحن نقدم خدمات استشارية تساعدكم على اختيار التأشيرة الصحيحة ونقدم المساعدة في استخراجها بسهولة ويسر.',
+      },
     },
     {
-      title: 'الزكاة و الضرائب و المعاملات المحاسبية',
-      image: '../assets/svg/taxes.svg',
-      description:
-        'المحاسبة والإدارة والتسويق هم الأعمدة الثلاثة الأساسية لأي عمل، ونحن نقدم خدمات متكاملة تغطي هذه الجوانب لتعزيز عملك.',
+      classes:
+        'px-5 py-10 text-sky-900 shadow-lg max-w-96 min-h-96 border border-slate-100',
+      content: {
+        imageSrc: '../assets/svg/taxes.svg',
+        imageAlt: 'taxes',
+        title: 'الزكاة و الضرائب و المعاملات المحاسبية',
+        description:
+          'المحاسبة والإدارة والتسويق هم الأعمدة الثلاثة الأساسية لأي عمل، ونحن نقدم خدمات متكاملة تغطي هذه الجوانب لتعزيز عملك.',
+      },
     },
   ];
+
+  offersItems: {
+    imageSrc: string;
+    imageAlt: string;
+    title: string;
+    description: string;
+  }[] = [
+    {
+      imageSrc: '../assets/svg/planing.svg',
+      imageAlt: 'planing',
+      title: 'التخطيط',
+      description: 'تحديد الخطوات اللازمة لتحقيق و الوصول للهدف المرجو.',
+    },
+    {
+      imageSrc: '../assets/svg/bank.svg',
+      imageAlt: 'bank',
+      title: 'فتح حساب بنكي',
+      description: 'للقيام بلعمليات المالية لاتمام اعمالك بشكل متكامل و سهل.',
+    },
+    {
+      imageSrc: '../assets/svg/establishing.svg',
+      imageAlt: 'establishing',
+      title: 'التأسيس',
+      description: 'تحديد الفكرة من خلال تحليل السوق ووضع خطة عمل.',
+    },
+    {
+      imageSrc: '../assets/svg/tracking.svg',
+      imageAlt: 'tracking',
+      title: 'المتابعة و التحسين',
+      description: 'تحديد الفكرة من خلال تحليل السوق ووضع خطة عمل.',
+    },
+  ];
+
+  constructor(private ngZone: NgZone) {
+    this.onResize();
+  }
+
+  @HostListener('window:resize')
+  private onResize(): void {
+    this.ngZone.run(() => {
+      if (typeof window !== 'undefined') {
+        this.isMenuOpen = window.innerWidth >= 768;
+      }
+    });
+  }
+
+  public toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
